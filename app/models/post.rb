919ad5fcc95346bfb872fb426ee43950e155c4ea
phillipnzau <nzauonline@gmail.com>
2023-08-17 16:52:17 +0300

    domne

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclution: {in: ['Fiction', 'Non-Fiction']}
+    validates :content, length: {minimum:100}
 end
